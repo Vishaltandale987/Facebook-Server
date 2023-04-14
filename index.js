0b1f029ff85eb527637491732cc3f34d33db44fd
Vishaltandale987 <vishaltandle800@gmail.com>
2023-04-14 19:33:45 +0530

    frist

--- index.js
+++ index.js
@@ -18,59 +18,20 @@ app.use(cors());
 
 
 
-app.use(fileupload({
-  useTempFiles: true
-}))
 
 
 
 
 
 
-cloudinary.config({
-  cloud_name: 'dd9cmhunr',
-  api_key: '358273696883359',
-  api_secret: '2brG2Fl46PgJ8DVDK8fFteaU420',
-  secure: true
-});
 
 
-app.post('/upload/:id', function  (req, res) {
-  let id=req.params.id
-  console.log(req.data)
-  // const finduser = UserModel.findById({_id:id})
-  const file = req.files.photo
-  let data = req.body.description
-
-  cloudinary.uploader
-    .upload(file.tempFilePath,(err,result ) => {
-      // finduser.getPopulatedPaths.
-      // let postdata = {post_image: "result.url", description:"data", like: Number,comment:Array}
-      console.log(result.url)
-      res.send({image:result.url ,description:data, id:id });
-
-    })
-});
 
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Learning Swagger for frist time",
-      version: "1.0.0",
-    },
-    servers: [
-      {
-        url: "http://localhost:8080",
-      },
-    ],
-  },
-  apis: ["./routes/*.js"],
-};
 
-const swaggerSpec = swaggerjsdoc(options)
-app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+
+
+
 
 app.use("/user", userrouter);
 app.use("/post", postrouter);
@@ -85,14 +46,6 @@ app.use("/post", postrouter);
 
 //get
 
-app.get("/", (req, res) => {
-  res.send("Welcome");
-});
-
-app.get("/upload", (req, res) => {
-  const notes =  find()
-  res.send(notes);
-});
 
 app.listen(process.env.port, async () => {
   try {
